13517c1ba14a5649a625620fff4b9bc2f80be4fc
Kai Starke-Naumann <kai.starke-naumann@gmx.net>
2023-07-24 17:04:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,55 @@
 // create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// create a router
-const router = express.Router();
-
-// use the router
-app.use(router);
-
-// create a route
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-// create another route
-router.get('/comments', (req, res) => {
-    res.send('Comments');
-});
-
-// start the server
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+// 1. create a server object
+// 2. listen on port 8080
+// 3. create a response header
+// 4. write a response to the client
+// 5. end the response
+// 6. start the server
+// 7. read a file
+// 8. split the file contents
+// 9. convert the file contents to a string
+// 10. create a response header
+// 11. write a response to the client
+// 12. end the response
+// 13. start the server
+// 14. create a server object
+// 15. listen on port 8080
+// 16. create a response header
+// 17. write a response to the client
+// 18. end the response
+// 19. start the server
+// 20. read a file
+// 21. split the file contents
+// 22. convert the file contents to a string
+// 23. create a response header
+// 24. write a response to the client
+// 25. end the response
+// 26. start the server
+// 27. create a server object
+// 28. listen on port 8080
+// 29. create a response header
+// 30. write a response to the client
+// 31. end the response
+// 32. start the server
+// 33. read a file
+// 34. split the file contents
+// 35. convert the file contents to a string
+// 36. create a response header
+// 37. write a response to the client
+// 38. end the response
+// 39. start the server
+// 40. create a server object
+// 41. listen on port 8080
+// 42. create a response header
+// 43. write a response to the client
+// 44. end the response
+// 45. start the server
+// 46. read a file
+// 47. split the file contents
+// 48. convert the file contents to a string
+// 49. create a response header
+// 50. write a response to the client
+// 51. end the response
+// 52. start the server
+// 53. create a server object
+// 54. listen on
